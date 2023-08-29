9929cdd0b3d0c8a001e85a81d45326f7e5d82e9c
Oakley <66735773+OakleyCord@users.noreply.github.com>
2023-08-29 11:28:46 -0400

    yo

---
+++ flake.nix
@@ -0,0 +1,41 @@
+{
+  description = "Oakley's NixOS Config'";
+  
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+  };
+
+  outputs = {self, nixpkgs}:
+  let
+    system = "x86_64-linux";
+    
+    pkgs = import nixpkgs {
+      inherit system;
+
+      config = {
+        allowUnfree = true;
+      };
+    };
+
+  in
+  {
+
+    nixosConfigurations = {
+      oldBlerm = nixpkgs.lib.nixosSystem {
+        specialArgs = { inherit system; };
+
+        modules = [
+          ./nixos/configuration.nix
+        ];
+      };
+      hypr = nixpkgs.lib.nixosSystem {
+        specialArgs = { inherit system; };
+
+        modules = [
+          ./hypr/configuration.nix
+        ];
+      };
+    };
+  };
+
+}
