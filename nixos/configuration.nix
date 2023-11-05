5fd0959010a066158b1a6afa9c9e969da89cf155
oakley <oakleycord@proton.me>
2023-11-05 11:46:07 -0500

    add gamemode

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -181,7 +181,28 @@
   services.tailscale.enable = true;
   programs.zsh.enable = true;
 
+  # gaming stuffs
   programs.steam.enable = true;
+  programs.gamemode.enable = true;
+  # fix gamescope on steam
+  nixpkgs.config.packageOverrides = pkgs: {
+    steam = pkgs.steam.override {
+      extraPkgs = pkgs: with pkgs; [
+        xorg.libXcursor
+        xorg.libXi
+        xorg.libXinerama
+        xorg.libXScrnSaver
+        libpng
+        libpulseaudio
+        libvorbis
+        stdenv.cc.cc.lib
+        libkrb5
+        keyutils
+      ];
+    };
+  };
+
+
 
   users.users.oakley = {
     isNormalUser = true;
@@ -225,22 +246,6 @@
   # Allow unfree packages
   nixpkgs.config.allowUnfree = true;
 
-  nixpkgs.config.packageOverrides = pkgs: {
-    steam = pkgs.steam.override {
-      extraPkgs = pkgs: with pkgs; [
-        xorg.libXcursor
-        xorg.libXi
-        xorg.libXinerama
-        xorg.libXScrnSaver
-        libpng
-        libpulseaudio
-        libvorbis
-        stdenv.cc.cc.lib
-        libkrb5
-        keyutils
-      ];
-    };
-  };
 
   nix.gc = {
     automatic = true;
