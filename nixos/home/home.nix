2f539130a0181f059e7f55d86c31c341799d4f28
oakley <oakleycord@proton.me>
2023-11-05 11:44:41 -0500

    fix gamescope on steam

--- nixos/home/home.nix
+++ nixos/home/home.nix
@@ -310,6 +310,15 @@
 
   home.sessionVariables = {
     EDITOR = "nvim";
+    # fix gamescope on steam
+    GDK_BACKEND = "wayland,x11";
+    QT_QPA_PLATFORM = "wayland;xcb";
+    #SDL_VIDEODRIVER = "x11";
+    CLUTTER_BACKEND = "wayland";
+    XDG_CURRENT_DESKTOP = "Hyprland";
+    XDG_SESSION_TYPE = "wayland";
+    XDG_SESSION_DESKTOP = "Hyprland";
+    WLR_NO_HARDWARE_CURSORS = "1";
   };
 
   home.stateVersion = "22.11"; # Please read the comment before changing.
