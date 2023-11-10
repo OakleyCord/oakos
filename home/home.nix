b4197a8bc8a0051f18bca33c41555c230108dbcc
oakley <oakleycord@proton.me>
2023-11-09 23:03:09 -0500

    initial move to swaync

--- home/home.nix
+++ home/home.nix
@@ -45,6 +45,9 @@
 
     # notify-send
     libnotify
+
+    # notifications
+    swaynotificationcenter
   ];
 
 
--- home/waybar/waybar.nix
+++ home/waybar/waybar.nix
@@ -36,7 +36,7 @@
       margin-right = 5;
       modules-left = ["hyprland/workspaces" "backlight" "pulseaudio" "network"];
       modules-center = ["clock"];
-      modules-right =  ["temperature" "cpu" "memory" "battery" "tray"];
+      modules-right =  ["temperature" "cpu" "memory" "battery" "custom/notification" "tray"];
       "wlr/workspaces" = {
         format = "{icon}";
         on-click = "activate";
@@ -113,6 +113,26 @@
         format = "{capacity}% (-{power}W) {icon} ";
         format-icons = ["" "" "" "" ""];
         format-charging = "{capacity}% (+{power}W) {icon}  ";
+      };
+      "custom/notification" = {
+        tooltip = false;
+        format = "{icon}";
+        format-icons = {
+          notification = "<span foreground='red'><sup></sup></span>";
+          none = "";
+          dnd-notification = "<span foreground='red'><sup></sup></span>";
+          dnd-none = "";
+          inhibited-notification = "<span foreground='red'><sup></sup></span>";
+          inhibited-none = "";
+          dnd-inhibited-notification = "<span foreground='red'><sup></sup></span>";
+          dnd-inhibited-none = "";
+        };
+        return-type = "json";
+        exec-if = "which swaync-client";
+        exec = "swaync-client -swb";
+        on-click = "swaync-client -t -sw";
+        on-click-right = "swaync-client -d -sw";
+        escape = true;
       };
       tray = {
         icon-size = 21;
