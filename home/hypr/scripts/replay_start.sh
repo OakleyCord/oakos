1f4f8332bcd428c73e62d65d5955ab774039fedf
oakley <oakleycord@proton.me>
2023-11-20 19:21:49 -0500

    Change up notify messages on replays

--- home/hypr/scripts/replay_start.sh
+++ home/hypr/scripts/replay_start.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
-notify-send Replay "Recorded last 30s"
 # TODO: add env variable for main monitor description so each host can define
 wl-screenrec -o $(hyprctl monitors -j | jq -r '.[] | select(.description | startswith("ASUSTek COMPUTER INC VG279QR M4LMQS260906")) | .name') --audio --audio-device replay-sink.monitor --history 30 -f ~/Videos/replays/REPLAY_$(date +%m-%d-%y_%T).mp4 &
 
