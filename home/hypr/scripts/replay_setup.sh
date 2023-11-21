2f6facb8f059870905667c58ba464e2b4fc4948b
oakley <oakleycord@proton.me>
2023-11-20 19:14:28 -0500

    fix jankyness on replay audio

--- home/hypr/scripts/replay_setup.sh
+++ home/hypr/scripts/replay_setup.sh
@@ -1,7 +1,23 @@
 #!/bin/sh
 
-# TODO: fix VERY rough way of getting mic and desktop audio 
-pw-link alsa_input.usb-Samson_Technologies_Samson_Go_Mic-00.analog-stereo:capture_FL alsa_output.usb-Samson_Technologies_Samson_Go_Mic-00.analog-stereo:playback_FL
-pw-link alsa_input.usb-Samson_Technologies_Samson_Go_Mic-00.analog-stereo:capture_FR alsa_output.usb-Samson_Technologies_Samson_Go_Mic-00.analog-stereo:playback_FR
-pw-link alsa_output.pci-0000_07_00.6.analog-stereo:monitor_FL alsa_output.usb-Samson_Technologies_Samson_Go_Mic-00.analog-stereo:playback_FL
-pw-link alsa_output.pci-0000_07_00.6.analog-stereo:monitor_FR alsa_output.usb-Samson_Technologies_Samson_Go_Mic-00.analog-stereo:playback_FR
+# TODO: trigger again automatically on changed defaults + remove previous links
+
+# remove previously created virtual sinks
+pactl unload-module module-null-sink
+
+# create a virtual sink
+pactl load-module module-null-sink media.class=Audio/Sink sink_name=replay-sink channel_map=stereo
+
+
+# map default sink and default source into virtual sink
+DEFAULT_SINK=$(pactl info | grep 'Default Sink' | cut -d':' -f 2)
+DEFAULT_SOURCE=$(pactl info | grep 'Default Source' | cut -d':' -f 2)
+
+
+
+
+pw-link ${DEFAULT_SOURCE}:capture_FL replay-sink:playback_FL
+pw-link ${DEFAULT_SOURCE}:capture_FR replay-sink:playback_FR
+
+pw-link ${DEFAULT_SINK}:monitor_FL replay-sink:playback_FL
+pw-link ${DEFAULT_SINK}:monitor_FR replay-sink:playback_FR
--- home/hypr/scripts/replay_start.sh
+++ home/hypr/scripts/replay_start.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 notify-send Replay "Recorded last 30s"
 # TODO: add env variable for main monitor description so each host can define
-wl-screenrec -o $(hyprctl monitors -j | jq -r '.[] | select(.description | startswith("ASUSTek COMPUTER INC VG279QR M4LMQS260906")) | .name') --audio --audio-device alsa_output.usb-Samson_Technologies_Samson_Go_Mic-00.analog-stereo.monitor --history 30 -f ~/Videos/replays/REPLAY_$(date +%m-%d-%y_%T).mp4 &
+wl-screenrec -o $(hyprctl monitors -j | jq -r '.[] | select(.description | startswith("ASUSTek COMPUTER INC VG279QR M4LMQS260906")) | .name') --audio --audio-device replay-sink.monitor --history 30 -f ~/Videos/replays/REPLAY_$(date +%m-%d-%y_%T).mp4 &
+
