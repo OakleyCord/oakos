554c319a4000109b2b3431a684247a503160a6b5
oakley <oakleycord@proton.me>
2024-06-03 19:10:55 -0400

    remove asus fan curves

--- modules/asus.nix
+++ modules/asus.nix
@@ -11,9 +11,6 @@
   services.asusd = {
     enable = true;
     enableUserService = true;
-    fanCurvesConfig = ''
-    ${builtins.readFile ./asus/fan_curves.ron}
-    '';
   };
 
   systemd.services.supergfxd.path = [ pkgs.pciutils ];
